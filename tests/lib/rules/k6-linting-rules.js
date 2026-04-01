3104da702377972c7d33ca4dcabbb8a1b7a7d244
Stuart Thomas <77386766+stuart-thomas-tqd@users.noreply.github.com>
2026-04-01 09:30:47 +0100

    Depednecy updates and test fixes

--- tests/lib/rules/k6-linting-rules.js
+++ tests/lib/rules/k6-linting-rules.js
@@ -4,28 +4,29 @@
  */
 "use strict";
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+const assert = require("assert");
+const ruleset = require("../../../lib/rules/k6-linting-rules");
 
-const rule = require("../../../lib/rules/k6-linting-rules"),
-  RuleTester = require("eslint").RuleTester;
+describe("k6-linting-rules", () => {
+  it("exports metadata and k6 rule overrides", () => {
+    assert.ok(ruleset.meta);
+    assert.strictEqual(
+      ruleset.meta.docs.description,
+      "Ruleset to support the linting of k6 tests",
+    );
 
+    assert.ok(ruleset.rules);
+    assert.ok(ruleset.rules.k6);
+  });
 
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
+  it("defines expected k6 eslint settings", () => {
+    const k6 = ruleset.rules.k6;
 
-const ruleTester = new RuleTester();
-ruleTester.run("k6-linting-rules", rule, {
-  valid: [
-    // give me some code that won't trigger a warning
-  ],
-
-  invalid: [
-    {
-      code: "This ruleset prevents eslint failing when linting k6",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
-    },
-  ],
+    assert.strictEqual(k6["import/no-unresolved"], 0);
+    assert.strictEqual(k6["no-restricted-globals"], 0);
+    assert.strictEqual(k6["import/extensions"], 0);
+    assert.strictEqual(k6["no-prototype-builtins"], 0);
+    assert.strictEqual(k6.semi, 1);
+    assert.strictEqual(k6.quotes, 2);
+  });
 });
